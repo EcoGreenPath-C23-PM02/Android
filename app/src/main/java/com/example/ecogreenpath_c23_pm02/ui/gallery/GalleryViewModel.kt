3872d68ce1a2d906ed384904a545b31c4b9e325d
AMR0017 <azhar.mr12@gmail.com>
2023-05-24 18:33:21 +0800

    first commit

---
+++ app/src/main/java/com/example/ecogreenpath_c23_pm02/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ecogreenpath_c23_pm02.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
