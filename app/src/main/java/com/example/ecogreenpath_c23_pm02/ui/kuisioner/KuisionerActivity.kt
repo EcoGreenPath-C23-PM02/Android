652d39b5897013c5cfdd3acf9ad859530f0e7ddb
Rizal Rahmadani <rizalbnt121@gmail.com>
2023-06-01 18:00:20 +0800

    Menambahakna halaman Kuisioner

---
+++ app/src/main/java/com/example/ecogreenpath_c23_pm02/ui/kuisioner/KuisionerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecogreenpath_c23_pm02.ui.kuisioner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecogreenpath_c23_pm02.R
+
+class KuisionerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kuisioner)
+    }
+}
